9409923b5ac7857a2bebf336be47f3465281a1ac
afzdev13 <joaosilvaafonso14@gmail.com>
2019-06-06 20:05:35 -0400

    feat: add image download logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customsearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -8,8 +9,10 @@ async function robot(){
     const content = state.load()
     
     await fetchimagesOfAllSentences(content)
-
+    await downloadAllImages(content)
     state.save(content)
+
+    
 
     async function fetchimagesOfAllSentences(content){
 		for(const sentence of content.sentences){
@@ -33,7 +36,38 @@ async function robot(){
             return item.link
         })
             return imagesUrl
-    }    
+    }
+    
+    async function downloadAllImages(content) {
+        content.downloadedImages = []
+        
+        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            const images = content.sentences[sentenceIndex].images
+
+            for(let imageIndex = 0; imageIndex < images.length; imageIndex++){
+                const imageUrl = images[imageIndex]
+            
+                try {
+                    if (content.downloadedImages.includes(imageUrl)){
+                        throw new error('Imagem ja baixada')
+                    }
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+                    content.downloadedImages.push(imageUrl)
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`)
+                    break
+                } catch(error) {
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
+                }
+            }          
+        }
+    }
+
+    async function downloadAndSave(url, fileName){
+        return imageDownloader.image({
+            url, url,
+            dest: `./content/${fileName}`
+        })
+    }
 }
 
 module.exports = robot
